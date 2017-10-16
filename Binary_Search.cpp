493b2835bf6d71e8ad2eac6b4ed0b0ceea1cb7c5
Next <next@next-hyd-684.nexteducation.com>
2017-10-16 11:16:35 +0530

    Code for Binary Search

---
+++ Binary_Search.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+#define loop(i,a,n) for(int i = a;i < n;i++)
+
+using namespace std;
+
+int Binary_search(vector <int> input, int toBeFound){
+    
+    int n = input.size(),num = 0;
+    int end = n-1; int start = 0;
+    while(start <= end){
+        
+        cout << "start " << start << " end " << end << endl;
+        double half =  start + (end - start) / 2;
+        cout << "half " << half << endl;
+        if(toBeFound >= input.at(half) ){
+            start = half;
+        }
+        else {
+            end = half;
+        }
+        
+        if(input.at(start) == toBeFound) return start;
+        if(input.at(end) == toBeFound) return end;
+
+    }
+    
+    return -1;
+}
+
+int main(){
+    
+    
+    int n = 0,k = 0;
+    cin >> n >> k;
+    vector <int> input(n);
+    loop(i,0,n){
+        cin >> input.at(i);
+    }
+    
+    cout << Binary_search(input,k);
+    
+}
