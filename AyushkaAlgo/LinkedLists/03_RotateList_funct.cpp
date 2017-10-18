301a30045f4f75f52377c98662247ad2afe842af
Cazaimi <root@next-hyd-684.nexteducation.com>
2017-10-18 10:24:34 +0530

    Incomplete solution to Rotating a Linked List

--- AyushkaAlgo/LinkedLists/03_RotateList_funct.cpp
+++ AyushkaAlgo/LinkedLists/03_RotateList_funct.cpp
@@ -1,58 +1,87 @@
-/*
-    
-  Rotate a Linked List (Function Problem)
-     
-  Given a singly linked list, rotate the linked list counter-clockwise by k nodes. Where k is a given positive integer smaller than or equal to length of the linked list. For example, if the given linked list is 10->20->30->40->50->60 and k is 4, the list should be modified to 50->60->10->20->30->40.
+     /*
 
-  Input:
+     Rotate a Linked List (Function Problem)
 
-  In this problem, complete the method which takes two argument: the head of the linked list and int k. We should not read any input from stdin/console.
-  The struct Node has a data part which stores the data and a next pointer which points to the next element of the linked list. 
-  There are multiple test cases. For each test case, this method will be called individually.
+     Given a singly linked list, rotate the linked list counter-clockwise by k nodes. Where k is a given positive integer smaller than or equal to length of the linked list.
+     For example, if the given linked list is 10->20->30->40->50->60 and k is 4, the list should be modified to 50->60->10->20->30->40.
 
-  Output:
-  Rotate the link list from index k and return its new head pointer.
+     Input:
 
+     In this problem, complete the method which takes two argument: the head of the linked list and int k. We should not read any input from stdin/console.
+     The struct Node has a data part which stores the data and a next pointer which points to the next element of the linked list. 
+     There are multiple test cases. For each test case, this method will be called individually.
 
-  Note: If you use "Test" or "Expected Output Button" use below example format
-
-  Example:
-  Input:
-  1
-  8
-  1 2 3 4 5 6 7 8
-  4
-
-  Output:
-  5 6 7 8 1 2 3 4
-*/
+     Output:
+     Rotate the link list from index k and return its new head pointer.
 
 
-/*
-Please note that it's Function problem i.e.
-you need to write your solution in the form of Function(s) only.
-Driver Code to call/invoke your function would be added by GfG's Online Judge.*/
+     Note: If you use "Test" or "Expected Output Button" use below example format
+
+     Example:
+     Input:
+     1
+     8
+     1 2 3 4 5 6 7 8
+     4
+
+     Output:
+     5 6 7 8 1 2 3 4
+     */
 
 
-/*
-  Rotate a linked list after node k
-  The input list will have at least one element  
-  Return pointer to head of rotated linked list 
-  Node is defined as 
-  struct node
-  {
+     /*
+     Please note that it's Function problem i.e.
+     you need to write your solution in the form of Function(s) only.
+     Driver Code to call/invoke your function would be added by GfG's Online Judge.*/
+
+
+     /*
+     Rotate a linked list after node k
+     The input list will have at least one element  
+     Return pointer to head of rotated linked list 
+     Node is defined as 
+     struct node
+     {
      int data;
      struct node *next;
-  }
-*/
+     }
+     */
 
 
 void rotate(struct node **head_ref, int k){ 
 
-     node ** current = head_ref;
-     int n = 1;
+     node * current = *head_ref;
+     node * new_head = nullptr;
+     node * new_headminus1 = nullptr;
+     int n = 0;
      while(current != nullptr){
-          current 
+          current = current -> next;
+          n++;
      }
+
+     //Now we know about the number of nodes in the linked list.
+
+     k = k % n;
+     current = head_ref;
+     int m = 1;
+     while(current -> next != nullptr){
+          current = current -> next;
+          m++;
+          if(m == k){
+               new_headminus1 = current;
+          }
+          if(m == k+1){
+               new_head = current;
+          }
+     }
+
+     //now, current holds the value of the last node.
+     //In 10-20-30-40-50-60, current is now 60.
+
+     current -> next = head_ref;
+     head_ref = new_head;
+     new_headminus1 -> next = nullptr;
+
+
 }
 
