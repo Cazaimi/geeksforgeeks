658b4d009a3ed08acd2f81be0ceaa2075615988c
Cazaimi <next@next-hyd-684.nexteducation.com>
2017-11-18 12:26:58 +0530

    BGGOnly. Maximum path in matrix.

--- AyushkaAlgo/DP/10_pathInMatrix.cpp
+++ AyushkaAlgo/DP/10_pathInMatrix.cpp
@@ -5,13 +5,68 @@
 
 using namespace std;
 
-int chkSurr(vector <vector <int> > matrix){
+
+int pathFromHere(vector <vector <int> > matrix, vector <vector <int> > &dp, int i, int j){
+	
+	int n = matrix.size();
+	
+	if(i < 0 or i >= n or j < 0 or j >= n) return 0;
+	
+	//cout << "Matrix[i][j]:" << matrix[i][j] << endl;
+	//cout << "Matrix[i][j]:" << matrix[i-1][j] << endl;
+	if(dp[i][j] != -1) return dp[i][j];
+	
+	
+	
+	if( i > 0 and matrix[i][j] == matrix[i-1][j]+1 ){
+		//cout << "FIRST i:" << i << " j:" << j << endl;
+		dp[i][j] = max(dp[i][j],1+pathFromHere(matrix,dp,i-1,j));
+		
+	}
+	if( i < n-1 and matrix[i][j] == matrix[i+1][j]+1){
+		//cout << "SECOND i:" << i << " j:" << j << endl;
+		dp[i][j] = max(dp[i][j],1+pathFromHere(matrix,dp,i+1,j));
+	}
+	if( j > 0 and matrix[i][j] == matrix[i][j-1]+1){
+		//cout << "THIRD i:" << i << " j:" << j << endl;
+		dp[i][j] = max(dp[i][j],1+pathFromHere(matrix,dp,i,j-1));
+	}
+	if( j < n-1 and matrix[i][j] == matrix[i][j+1]+1){
+		//cout << "FOURTH i:" << i << " j:" << j << endl;
+		dp[i][j] = max(dp[i][j],1+pathFromHere(matrix,dp,i,j+1));
+	}
+	dp[i][j] = max(dp[i][j],1) ;
+	return dp[i][j];
 	
 }
 
-int path(vector <vector <int> > matrix, vector <vector <int> > dp){
+int longestpath(vector <vector <int> > matrix ){
 	
+	int n = matrix.size();
 	
+	vector <vector <int> > dp;
+	loop(i,0,n){
+ 		vector <int> row;
+ 		loop(j,0,n) row.push_back(-1);
+ 		dp.push_back(row);
+	}
+	
+	int maxx = 0;
+	loop(i,0,n){
+		loop(j,0,n){
+			//cout << "Calling i=" << i << " & j=" << j << endl; 
+			maxx = max(maxx,pathFromHere(matrix,dp,i,j));
+		}
+	}
+	
+	loop(i,0,n){
+		loop(j,0,n){
+			cout << dp[i][j] << " ";
+		}
+		cout << endl;
+	}
+	
+	return maxx;
 }
 
 int main(){
@@ -21,21 +76,18 @@ int main(){
 	loop(i,0,t){
 		int n = 0;
 		cin >> n;
-		vector <vector <int> > matrix,<vector <int> > dp;
+		vector <vector <int> > matrix;
 		loop(i,0,n){
-			vector <int> row, <int> auxrow;
+			vector <int> row;
 			loop(j,0,n){ 
 				int temp = 0;
 				cin >> temp;
-				row.push_back(temp);
-				auxrow.push_back(1);
-				
+				row.push_back(temp);			
 			}
 			matrix.push_back(row);
-			dp.push_back(auxrow);
 		}
 		
-		int path(matrix,dp);
+		cout << longestpath(matrix) << endl;
 	}
 	return 0;
 }
