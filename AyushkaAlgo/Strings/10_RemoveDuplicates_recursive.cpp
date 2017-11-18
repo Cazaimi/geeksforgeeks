5eefed03a2e7d055e53402c26912e1a2ac95ff7d
Cazaimi <next@next-hyd-684.nexteducation.com>
2017-11-18 10:57:29 +0530

    Incomplete. Recusive solution. TBD.

---
+++ AyushkaAlgo/Strings/10_RemoveDuplicates_recursive.cpp
@@ -0,0 +1,23 @@
+
+Remove Duplicates
+
+
+
+
+Given a string, remove duplicates from it. Note that original order of characters must be kept same.  Expected time complexity O(n) where n is length of input string and extra space O(1) under the assumption that there are total 256 possible characters in a string.
+
+Input: First line of the input is the number of test cases T. And first line of test case contains a string.
+
+Output:  Modified string without duplicates and same order of characters.
+
+Constraints:  Input String Length <= 1000
+
+Example:
+Input:
+2
+geeksforgeeks
+geeks for geeks
+
+Output:
+geksfor
+geks for
