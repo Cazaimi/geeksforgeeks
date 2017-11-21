841b5a060fd70f09615a9b9c8bbac595290e3fc8
Cazaimi <next@next-hyd-684.nexteducation.com>
2017-11-21 19:21:29 +0530

    Incomplete.

---
+++ AyushkaAlgo/DP/15_ShrtstCmnSprSq.cpp
@@ -0,0 +1,21 @@
+	
+Shortest Common Supersequence
+Given two strings str1 and str2, find the shortest common shortest common subsequence of the two strings.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases.Each test case contains two space separated strings.
+
+Output:
+Output the length of the shortest common supersequence.
+
+Constraints:
+1<=T<=100
+
+Example:
+Input:
+2
+abcd xycd
+efgh jghi
+Output:
+6
+6
