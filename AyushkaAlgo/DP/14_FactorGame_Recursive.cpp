5b9892ab3f4b0b3fcc629ae6ff85c02a3f03fc45
Cazaimi <anmol01shukla@gmail.com>
2017-11-21 09:38:25 +0530

    One incomplete.

---
+++ AyushkaAlgo/DP/14_FactorGame_Recursive.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+#define loop(i,a,n) for(int i = a;i < n;i++)
+
+using namespace std;
+
+int game(int n, int turn){
+    
+    //cout << "(" << n << "," << turn%2 << ")" << endl;
+	//base cases.
+	if(n <= 0){
+		//cout << "being called!\n" ;
+		if(turn % 2 == 0)
+		return 0;
+		else return 1;  
+	}
+	
+	int winner = (turn + 1) % 2;
+	for(int i = 1;i < n; i++){
+		if(n % i == 0){
+			//cout << "n:" << n << " i:" << i << endl;
+			winner = game(n - i,turn+1);
+			if(winner != turn % 2) continue;
+			else break;
+		}
+	}
+
+	return winner;
+
+}
+
+int main()
+ {
+	//code
+	int t = 0;
+	cin >> t;
+	loop(j,0,t){
+	
+	    int n = 0;
+	    cin >> n;
+	    cout << game(n,1) << endl;
+	}
+	return 0;
+}
