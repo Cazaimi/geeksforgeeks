bafe4ab34d996957bf5c402c4f546a25cd6cdefa
Cazaimi <anmol01shukla@gmail.com>
2017-11-22 06:47:43 +0530

    By God's grace only. Solution is LCSubSequence(not SubString). An end to this problem and to the DP set.

--- AyushkaAlgo/DP/15_ShrtstCmnSprSq.cpp
+++ AyushkaAlgo/DP/15_ShrtstCmnSprSq.cpp
@@ -1,5 +1,4 @@
-	
-Shortest Common Supersequence
+/* Shortest Common Supersequence
 Given two strings str1 and str2, find the shortest common shortest common subsequence of the two strings.
 
 Input:
@@ -19,3 +18,68 @@ efgh jghi
 Output:
 6
 6
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#define loop(i,a,n) for(int i = a;i < n; i++)
+
+using namespace std;
+
+
+int lcs(string a, string b){
+	
+	int n = a.length(), m = b.length(), maxx = 0;
+	int dp[n+1][m+1];
+	
+	
+	for(int i = 0;i <= n; i++){
+		for(int j = 0;j <= m; j++){
+			//cout << i << " " << j << endl;
+			if(i == 0 or j == 0){
+				dp[i][j] = 0;
+			}
+			else if(a.at(i-1) == b.at(j-1)){
+				dp[i][j] = dp[i-1][j-1] + 1;
+			}
+			else{
+				dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+			}
+			
+			maxx = max(maxx,dp[i][j]);
+		}
+	}
+	
+	
+	/*loop(i,0,n+1){
+		loop(j,0,m+1){
+			cout << dp[i][j] << " ";
+		}
+		cout << endl;
+	}
+	//cout << maxx << endl; */
+	
+	return maxx;
+}
+
+int rf(string a, string b){
+	
+	int same = lcs(a,b);
+	//cout << same << endl;
+	int different = (a.length() - same) + b.length();
+	return different; 
+}
+
+int main(){
+	
+	int t =0;
+	cin >> t;
+	loop(i,0,t){
+		string a,b;
+		cin >> a >> b;
+		cout << rf(a,b) << endl;
+	}
+
+}
